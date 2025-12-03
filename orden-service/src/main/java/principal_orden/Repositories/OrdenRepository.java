93e9c5b5e54fa70d9fa3d0eefdb66b01056eb575
rusok <rusok.software@gmail.com>
2025-12-03 14:40:18 -0400

    Trazas

--- orden-service/src/main/java/principal_orden/Repositories/OrdenRepository.java
+++ orden-service/src/main/java/principal_orden/Repositories/OrdenRepository.java
@@ -1,7 +1,10 @@
 package principal_orden.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import principal_orden.Models.Orden;
 
 public interface OrdenRepository  extends JpaRepository<Orden,Long> {
+    List<Orden> findByTotal(Double total);
 }
--- orden-service/src/main/java/principal_orden/Services/OrdenService.java
+++ orden-service/src/main/java/principal_orden/Services/OrdenService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import principal_orden.DTO.Producto;
 import principal_orden.Models.Orden;
 import principal_orden.Repositories.OrdenRepository;
@@ -18,6 +20,7 @@ public class OrdenService {
     @Autowired
     private RestTemplate restTemplate;
     private static final Logger logger = LoggerFactory.getLogger(OrdenService.class);
+    @CircuitBreaker(name = "productServiceCB", fallbackMethod = "fallbackGetProduct")
     public Orden realizarOrden(Orden order) {
 
         // Llamada al microservicio de productos
@@ -34,5 +37,14 @@ public class OrdenService {
         logger.debug("getAllOrdenes");
         logger.info("Retornando todas las ordenes");
         return ordenRepository.findAll();
+    }
+    public Orden fallbackGetProduct(Orden o, Throwable throwable) {
+        System.out.println("Fallback originado por: " + throwable.getMessage());
+        // Aquí incluso podría lanzar una excepción específica
+        Orden fallbackOrder = new Orden();
+        fallbackOrder.setProductoId(o.getProductoId());
+        fallbackOrder.setQuantity(o.getQuantity());
+        fallbackOrder.setTotal(-1.0); // o algún valor por defecto
+        return fallbackOrder;
     }
 }
